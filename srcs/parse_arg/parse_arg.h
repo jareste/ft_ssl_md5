39584bd281bd63d6b3dec9be7bd1b37ba5849123
jareste <jareste2000@gmail.com>
2024-08-18 21:04:01 +0200

    something was wrongly tabbed

--- srcs/parse_arg/parse_arg.h
+++ srcs/parse_arg/parse_arg.h
@@ -1,7 +1,6 @@
 #ifndef PARSE_ARG_H
 #define PARSE_ARG_H
 
-void parse_args(int argc, char *argv[], int *flags, void** encrypt, algorithms
-* algorithm);
+void parse_args(int argc, char *argv[], int *flags, void** encrypt, algorithms* algorithm);
 
 #endif
